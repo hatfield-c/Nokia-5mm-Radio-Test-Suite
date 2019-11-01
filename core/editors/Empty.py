313bb06bc3fa2845d78b5368f8edf16b53f54082
Cody Hatfield <hatfield.coders@gmail.com>
2019-11-01 05:27:47 -0500

    [NOK-13] Upon first run the application will display welcome message

--- core/editors/Empty.py
+++ core/editors/Empty.py
@@ -1,6 +1,18 @@
 from Editor import Editor
+import tkinter
 
 class Empty(Editor):
-    def __init__(self, title = "", root = None, color = None):
-        super().__init__(title = title, root = root)
-        self.config(background = color)
+    def __init__(self, root = None):
+        super().__init__(title = "", root = root)
+        
+        self.container = tkinter.Frame(self, width = self.dimensions["width"], height = self.dimensions["height"])
+
+        # Make your changes here, 
+        ##########################################
+        self.deleteThis = tkinter.Label(self.container, text = "WELCOME YO")
+
+        self.deleteThis.pack()
+        ##########################################
+
+        self.container.grid(row = 0, column = 0, pady = 20, padx = 20)
+
