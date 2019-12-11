46913b150edfd330297ffaf3ee5d3612dfbac0cb
Cody Hatfield <hatfield.coders@gmail.com>
2019-12-11 03:45:00 -0600

    Standardizing Python Distribution

--- run.bat
+++ run.bat
@@ -1,1 +1,7 @@
+
+if NOT exist "%~dp0\Python\python.exe" (
+    start "" cmd /c "echo.&echo.&echo Python was not detected!&echo.&echo Please try running 'reinstall.bat' to install a local Python instance, or install one yourself in the 'Python/' folder.&echo.&echo.&echo(&pause"
+    exit /b
+)
+
 .\Python\python.exe .\Application.py
